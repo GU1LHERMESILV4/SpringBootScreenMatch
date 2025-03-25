22bb8e638f548dfc5f67d888e097d6c063c7b4ce
Guilherme Antonio Silva <guiantoni596@gmail.com>
2025-03-25 17:02:00 -0300

    Criação de construtores Série

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -2,6 +2,9 @@ package br.com.alura.screenmatch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Optional;
+import java.util.OptionalDouble;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Serie {
     private String titulo;
@@ -11,4 +14,14 @@ public class Serie {
     private String atores;
     private String poster;
     private String sinopse;
+
+    public Serie(DadosSerie dadosSerie){
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
 }
