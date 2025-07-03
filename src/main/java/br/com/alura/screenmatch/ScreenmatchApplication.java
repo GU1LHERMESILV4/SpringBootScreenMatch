a67887064926f35a78835a8fad620e7fcca4ba33
Guilherme Antonio Silva <102996571+GU1LHERMESILV4@users.noreply.github.com>
2025-07-02 23:50:27 -0300

    refactor: ajustar ScreenmatchApplication para injeção de dependência do SerieRepository

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,30 +1,25 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.principal.Principal;
-import br.com.alura.screenmatch.service.SerieService;
-import br.com.alura.screenmatch.service.traducao.TranslationService;
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
+
+    @Autowired
+    private SerieRepository repositorio;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner run(SerieService serieService) {
-		return args -> {
-			Principal principal = new Principal(serieService); // Agora passamos o serviço corretamente
-			principal.exibeMenu();
-		};
-	}
-
-	@Bean
-	public SerieService serieService() {
-		return new SerieService(new TranslationService()); // Criando dependências corretamente
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
 	}
 }
